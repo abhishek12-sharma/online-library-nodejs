9a5dc887baaeee2075ef04168105136362e56009
Abhishek Sharma <amansharma120704@gmail.com>
2025-11-20 22:56:07 +0530

    Use env vars for DB config

--- db.js
+++ db.js
@@ -1,11 +1,10 @@
-// db.js
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",        // change if needed
-  password: "Aman@15464",        // put your MySQL password
-  database: "library_db"
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "Aman@15464",
+  database: process.env.DB_NAME || "library_db"
 });
 
 module.exports = pool.promise();
